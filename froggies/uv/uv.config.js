fb0db03da9e79d2850373cc3fa4dd42f5a84a84e
MrMacAttaxy <116235140+MrMacAttaxy@users.noreply.github.com>
2024-12-06 15:26:11 -0500

    Update uv.config.js

--- froggies/uv/uv.config.js
+++ froggies/uv/uv.config.js
@@ -3,8 +3,8 @@ self.__uv$config = {
     bare:'hhttps://bare.frogiesarcade.workers.dev/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/static/uv/uv.handler.js',
-    bundle: '/static/uv/uv.bundle.js',
-    config: '/static/uv/uv.config.js',
-    sw: '/static/uv/uv.sw.js',
+    handler: '/froggies/uv/uv.handler.js',
+    bundle: '/froggies/uv/uv.bundle.js',
+    config: '/froggies/uv/uv.config.js',
+    sw: '/froggies/uv/uv.sw.js',
 };
